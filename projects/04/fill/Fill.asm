c534ffe6f071dcab7666b53bdb01537fba5a6f06
Tuan Nguyen <tuan.nguyenviet271@gmail.com>
2022-05-24 21:30:05 +0700

    fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,46 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// (game_loop):
+//   color = 0
+//   ram[kbd] == 0: jump draw
+//   color = -1
+//   (draw_loop)
+//   if i == kbd: jump stop
+//   ram[i] = color
+//   i += 1
+
+(GAME_LOOP)
+@SCREEN
+D=A
+@i
+M=D // i = SCREEN
+@color
+M=0 // WHITE
+@KBD
+D=M
+@draw_loop
+D;JEQ
+@color
+M=-1 // BLACK
+  (DRAW_LOOP)
+  @i
+  D=M
+  @KBD
+  D=D-A
+  @STOP
+  D;JEQ
+  @color
+  D=M
+  @i
+  A=M
+  M=D // RAM[i] = COLOR
+  @i
+  M=M+1
+  @DRAW_LOOP
+  0;JMP
+
+(STOP)
+@GAME_LOOP
+0;JMP
